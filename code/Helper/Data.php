94cd83ce06d1d14573bebea41643883d65f6843c
root <root@hosting>
2014-09-15 15:12:48 +0000

    first commit

---
+++ code/Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WebFlakeStudio
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://webflakestudio.com/WFS-LICENSE-COMMUNITY.txt
+ *
+ *
+ * MAGENTO EDITION USAGE NOTICE
+ *
+ * This package designed for Magento COMMUNITY edition
+ * WebFlakeStudio does not guarantee correct work of this extension
+ * on any other Magento edition except Magento COMMUNITY edition.
+ * WebFlakeStudio does not provide extension support in case of
+ * incorrect edition usage.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future.
+ *
+ * @category   Wfs
+ * @package    Wfs_DisableCompare
+ * @copyright  Copyright (c) 2013 WebFlakeStudio (http://webflakestudio.com)
+ * @license    http://webflakestudio.com/WFS-LICENSE-COMMUNITY.txt
+ */
+class Wfs_DisableCompare_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
